9d298de9373e70c6f7ac3ac39bde6f23ae675e56
buttonwood <tanhao2013@foxmail.com>
2014-07-27 22:01:37 +0800

    Update
    Fixed

--- perl/plot.pl
+++ perl/plot.pl
@@ -24,9 +24,9 @@ while(<IN>){
 	next if (/#/);
 	next if (/^$/);
     my @t = split;
-    $tar{$t[0]} = $t[5];
-    $que{$t[6]} = $t[11];
-    push @{$hash{$t[0]}{$t[6]}}, [$t[2],$t[5],$t[8],$t[9],$t[5],$t[11],$t[10]];
+    $tar{$t[0]} = $t[5];  #target length
+    $que{$t[6]} = $t[11]; #queru length
+    push @{$hash{$t[0]}{$t[6]}}, [$t[2],$t[3],$t[8],$t[9],$t[5],$t[11],$t[10]]; #alignments
 }
 close IN;
 
@@ -56,18 +56,17 @@ my $idx = 0;
 my $tmp  = 10;
 foreach (sort{$tar{$b} <=> $tar{$a}} (keys %tar)){
     my $y = $tar{$_} * $scale;
-    $tar{$_} = [$tmp,$y];
+    $tar{$_} = [$tmp,$y]; # target location [st, ed]
     my $col = $col_tab[$idx % ($#col_tab + 1)]; $idx++;
     $svg->rect(x => $tmp, y => 200, width => $y, height => 10,
             rx => 2.5, ry => 2.5, style => "fill:$col");
     $tmp += $y + $region;
-
 }
 
 $tmp = 10;
 foreach (sort{$que{$b} <=> $que{$a}} (keys %que)){
 	my $y = $que{$_} * $scale;
-	$que{$_} = [$tmp,$y];
+	$que{$_} = [$tmp,$y]; # query location [st, ed]
     my $col = $col_tab[$idx % ($#col_tab + 1)]; $idx++;
     $svg->rect(x => $tmp, y => 350, width => $y, height => 10,
             rx => 2.5, ry => 2.5, style => "fill:$col");
@@ -84,14 +83,14 @@ foreach my $t (keys %hash) {
         #print Dumper($tg);
         foreach my $tg (@{$hash{$t}{$q}}) {
             #print Dumper($tg);
-            my $ts = $tg->[0];
-            my $te = $tg->[1];
-            my $qs = $tg->[2];
-            my $qe = $tg->[3];
-            my $tl = $tg->[4];
+            my $ts = $tg->[0]; # target alignment start
+            my $te = $tg->[1]; # target alignment end
+            my $qs = $tg->[2]; # query alignment start
+            my $qe = $tg->[3]; # query alignment end
+            my $tl = $tg->[4]; # target length
             #print $tl,"\n";
-            my $ql = $tg->[5];
-            my $sd = $tg->[6]; # strand;
+            my $ql = $tg->[5]; # query length
+            my $sd = $tg->[6]; # alignment strand;
             my $style = "";
             if ($sd eq "+"){
                 $style = "stroke:rgb(70,130,180)"; # SteelBlue
@@ -130,3 +129,7 @@ sub sum{
     }
     return $s;
 }
+
+
+#les ../lastz.tab |grep -v "#" |awk 'BEGIN{OFS="\t"}{if($11=="+"){print "linkid"NR,$1,$3,$4"color=blue\nlinkid"NR,$7,$9,$10,"color=blue"}else{print "linkid"NR,$1,$3,$4"color=pink\nlinkid"NR,$7,$9,$10,"color=pink"}}'|les >links.txt
+# les ../lastz.tab |grep -v "#" |awk 'BEGIN{OFS="\t"}{if($11=="+"){print "linkid"NR,$1,$3,$4"\nlinkid"NR,$7,$9,$10,"color=blue"}else{print "linkid"NR,$1,$3,$4"\nlinkid"NR,$7,$9,$10,"color=pink"}}'|les >links.txt
